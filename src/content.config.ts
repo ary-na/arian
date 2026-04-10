81c9529537544d5afeb76caf700ee4d168263af1
Arian N. Yamchelo <ariannajafi@icloud.com>
2026-04-10 16:30:05 +1000

    add hero image alt and tags to the blog collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,18 +3,20 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-			category: z.array(z.string()).optional(),
-		}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.optional(image()),
+      heroImageAlt: z.string().optional(),
+      category: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+    }),
 });
 
 export const collections = { blog };
